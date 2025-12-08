58b9f4282fe7379a6e0e55cff23a89794821d63a
Vipasha-aft12 <vipasha@affluencetravels.com>
2025-12-08 13:48:50 +0530

    package changes for surya

--- src/component/packagelpdestination/PackageLpDestination.jsx
+++ src/component/packagelpdestination/PackageLpDestination.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import './PackageLpDestination.css';
 
 
 const PackageLpDestination = () => {
   return (
     
-      <div className="pkg_landingp_destination-box cent">
-        <Link href="/">
+      <div className="pkg-lp-desti-box cent fd-col">
             <Image
-              src="/images/package-page/package-images/bali.webp"
-              className="img-fluid"
-              width={401}
-              height={542}
+              src="/images/package-page/package-images/visa-free-2.webp"
+              className="img-fluid pkg-lp-desti-box-img"
+              width={601}
+              height={521}
               alt="bali" />
-        </Link>
+            <p className='pkg-lp-desti-box-price w-100'>Start From $499</p>
+            <div className='pkg-lp-desti-box-content cent w-100 fd-col'>
+              <p><strong>Bali</strong></p>
+              <p>12 Packages available</p>
+            </div>
       </div>
      
   )
